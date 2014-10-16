c38b7089c26ad1bb00723cbe6571059530df7aee
Conaaando <conaaando@gmail.com>
2014-10-16 19:25:13 -0300

    Added structure for Countdown Pal, day 10.

---
+++ Day 10/Countdown Pal/Countdown PalTests/Countdown_PalTests.swift
@@ -0,0 +1,36 @@
+//
+//  Countdown_PalTests.swift
+//  Countdown PalTests
+//
+//  Created by Fernando Fernandes on 10/16/14.
+//  Copyright (c) 2014 Blue Spell. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Countdown_PalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
